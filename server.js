75665b3a71449a883719c0b88ad3a5091ab54ec0
Bridget Asser <bridgetasser@gmail.com>
2020-01-06 14:55:55 -0500

    required and configured Passport, created User model, passport logic for handling and rendering the login and log out form

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var Location = require("./models/location");
-var Comment = require("./models/comment");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStategy = require("passport-local");
+const Location = require("./models/location");
+const User = require("./models/user");
+const Comment = require("./models/comment");
 // seedDB = require("./seeds");
 require("./config/database");
 
@@ -12,6 +15,31 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 // seedDB();
 
+//PAssport Config
+
+app.use(
+  require("express-session")({
+    secret: "Vanilla is better than chocolate",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.render("welcome");
+});
+
+//Root Route
 app.get("/", function(req, res) {
   res.render("welcome");
 });
@@ -79,7 +107,6 @@ app.get("/locations/:id/comments/new", function(req, res) {
 });
 
 app.post("/locations/:id/comments", function(req, res) {
-  //lookup campground using ID
   Location.findById(req.params.id, function(err, location) {
     if (err) {
       console.log(err);
@@ -97,6 +124,55 @@ app.post("/locations/:id/comments", function(req, res) {
     }
   });
 });
+
+////AUTHH RPUTES
+
+app.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+//handle sign in logic
+app.post("/signup", function(req, res) {
+  let newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("signup");
+    }
+    passport.authenticate("local")(req, res, function() {
+      res.redirect("/locations");
+    });
+  });
+});
+
+///log in form
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+//handling login logic
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/locations",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
+
+//logic route
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/locations");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthentcated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, function() {
   console.log("The Nap App Server is Now Sleep Walking!");
