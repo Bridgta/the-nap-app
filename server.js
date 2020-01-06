f653d84c9852969c6b5cf933ef0c7fea6da8ca1e
Bridget Asser <bridgetasser@gmail.com>
2020-01-06 10:25:17 -0500

    set up location schema and cont render views

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 // SCHEMA SETUP
-var locationsSchema = new mongoose.Schema({
+const locationsSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String
 });
 
-const locations = mongoose.model("Location", locationSchema);
+const Location = mongoose.model("Location", locationsSchema);
 
 app.get("/", function(req, res) {
   res.render("welcome");
@@ -22,7 +22,13 @@ app.get("/", function(req, res) {
 
 //INDEX - show all locations
 app.get("/locations", function(req, res) {
-  res.render("locations", { locations: locations });
+  Location.find({}, function(err, allLocations) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render.apply("index", { locations: allLocations });
+    }
+  });
 });
 
 //Create - create all locations
@@ -30,14 +36,35 @@ app.post("/locations", function(req, res) {
   // get data from form and add to locations array
   var name = req.body.name;
   var image = req.body.image;
-  var newLocation = { name: name, image: image };
-  locations.push(newLocation);
-  //redirect back to campgrounds page
-  res.redirect("/Locations");
+  var desc = req.body.description;
+  var newLocation = { name: name, image: image, description: desc };
+  // Create a new campground and save to DB
+  Location.create(newLocation, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      //redirect back to campgrounds page
+      res.redirect("/locations");
+    }
+  });
 });
 
+//NEW
 app.get("/locations/new", function(req, res) {
   res.render("new.ejs");
+});
+
+//SHOW - shows more info about a location
+app.get("/locations/:id", function(req, res) {
+  //find the campground with provided ID
+  Location.findById(req.params.id, function(err, foundLocation) {
+    if (err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      res.render("show", { location: foundLocation });
+    }
+  });
 });
 
 app.listen(3000, function() {
