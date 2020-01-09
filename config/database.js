10abf1a15d8b7f369b70b3744cab8a962ce6f11b
Bridget Asser <bridgetasser@gmail.com>
2020-01-09 13:08:39 -0500

    mongo Atlas set up for deployed database

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ var mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-  // useCreateIndex: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 // shortcut to mongoose.connection object
